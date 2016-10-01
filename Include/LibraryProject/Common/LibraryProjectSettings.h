56a3c010f2991c08be79d08ac2d18fa30a599a0a
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2016-10-01 10:33:19 +0900

    ヘッダファイルを追加

---
+++ Include/LibraryProject/Common/LibraryProjectSettings.h
@@ -0,0 +1,27 @@
+//  -*-  coding: utf-8; mode: c++  -*-  //
+/*************************************************************************
+**                                                                      **
+**                      ---  Library Project.  ---                      **
+**                                                                      **
+**          Copyright (C), 2016, Takahiro Itou                          **
+**          All Rights Reserved.                                        **
+**                                                                      **
+*************************************************************************/
+
+/**
+**      プロジェクトの設定。
+**
+**      @file       Common/LibraryProjectSettings.h
+**/
+
+#if !defined( LIBPROJ_COMMON_INCLUDED_LIBRARY_PROJECT_SETTINGS_H )
+#    define   LIBPROJ_COMMON_INCLUDED_LIBRARY_PROJECT_SETTINGS_H
+
+//  スクリプトによる設定値が書き込まれたヘッダを読み込む。  //
+#include    "LibraryProject/.Config/ConfiguredLibraryProject.h"
+
+LIBPROJ_NAMESPACE_BEGIN
+
+LIBPROJ_NAMESPACE_END
+
+#endif
