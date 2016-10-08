074f50dcfb403f7209b17143aea55244a7627609
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2016-10-08 18:56:50 +0900

    モジュールの実装ファイルを仮作成

---
+++ Lib/Foo/Foo.cpp
@@ -0,0 +1,78 @@
+//  -*-  coding: utf-8; mode: c++  -*-  //
+/*************************************************************************
+**                                                                      **
+**                      ---  Library Project.  ---                      **
+**                                                                      **
+**          Copyright (C), 2016, Takahiro Itou                          **
+**          All Rights Reserved.                                        **
+**                                                                      **
+*************************************************************************/
+
+/**
+**      An Implementation of Foo class.
+**
+**      @file       Foo/Foo.h
+**/
+
+#include    "LibraryProject/Foo/Foo.h"
+
+#include    "LibraryProject/Bar/Bar.h"
+
+LIBPROJ_NAMESPACE_BEGIN
+namespace  Foo  {
+
+//========================================================================
+//
+//    Foo  class.
+//
+
+//========================================================================
+//
+//    Constructor(s) and Destructor.
+//
+
+//========================================================================
+//
+//    Public Member Functions (Implement Pure Virtual).
+//
+
+//========================================================================
+//
+//    Public Member Functions (Overrides).
+//
+
+//========================================================================
+//
+//    Public Member Functions (Pure Virtual Functions).
+//
+
+//========================================================================
+//
+//    Public Member Functions (Virtual Functions).
+//
+
+//========================================================================
+//
+//    Public Member Functions.
+//
+
+CommonType
+Foo::functionFoo(
+        CommonType  x,
+        CommonType  y)
+{
+    return ( 0 );
+}
+
+//========================================================================
+//
+//    Protected Member Functions.
+//
+
+//========================================================================
+//
+//    For Internal Use Only.
+//
+
+}   //  End of namespace  Foo
+LIBPROJ_NAMESPACE_END
